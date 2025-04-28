8cc2cdad05427a0433247ced45158bb599cdaf8f
SaraBarrett <sara.c.mon@gmail.com>
2025-04-28 10:47:08 +0100

    data types

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Olá Turma de Ui UX");
+let myClass = "Turma UI/UX";
+let myName = "Sara";
+
+//alert("Olá Turma de Ui UX");
+
+//sem login
+//alert("Olá");
+
+//c/ login
+alert("Olá " + myClass);
+alert(myClass + " estão a ir muito bem!");
+alert("As notas da " + myClass + " são");
