195fe7ffb8f18a8b33f796c4f99f3b3f2880ac9c
SaraBarrett <sara.c.mon@gmail.com>
2025-04-29 16:56:57 +0100

    numbers

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//Aqui vão estar todos os exs da aula
+//pp16
+let myLuckyNumber = 16;
+let myName = "Sara";
+
+alert("o meu nome é "+myName + " e o meu número preferido é o " + myLuckyNumber);
--- js/script.js
+++ js/script.js
@@ -1,12 +1,50 @@
 let myClass = "Turma UI/UX";
-let myName = "Sara";
+let myLoginName = null;
 
-//alert("Olá Turma de Ui UX");
+const MYBIRTHDAY = 1986;
 
-//sem login
-//alert("Olá");
+let myNumber = 6;
 
-//c/ login
-alert("Olá " + myClass);
-alert(myClass + " estão a ir muito bem!");
-alert("As notas da " + myClass + " são");
+// myNumber /= 2;
+// myNumber += 10;
+// alert(myNumber);
+// //alert("Olá Turma de Ui UX");
+// alert("Olá " + myLoginName);
+// //sem login
+// //alert("Olá");
+// myLoginName = "Sara";
+// //c/ login
+// alert("Olá " + myLoginName);
+// alert(myClass + " estão a ir muito bem!");
+// alert("As notas da " + myClass + " são");
+
+// myLoginName = "Nuno";
+// alert("Olá " + myLoginName);
+
+//exemplo actualizar variáveis: carrinho de compras
+// let myCart = 0;
+// alert("neste momento tens a pagar: " + myCart);
+
+// //comprei chocolate que custa 2euros
+// myCart = myCart + 2;
+// alert("depois de comprar chocolates: " + myCart);
+
+// //comprei arroz que custa 0.7
+// myCart = myCart + 0.7;
+// alert("depois de comprar arroz para fazer o jantar: " + myCart);
+
+// //arrependi-me do chocolate e tirei do carrinho
+// myCart = myCart - 2;
+// alert("depois de retirar o chocolate tenho: " + myCart);
+
+// //adicionei um gerador que custa 500
+// myCart = myCart + 500;
+// alert("depois de ter energia em casa pago: " + myCart);
+
+// //foi-me aplicado devido à crise de energia um desconto de 5%
+// myCart = myCart - myCart * 0.05;
+// alert("total com desconto: " + myCart);
+
+let moduleName = "Tecnicas Avançadas de Programação";
+alert("A primeira letra do módulo é " + moduleName[0]);
+alert(moduleName[8]);
